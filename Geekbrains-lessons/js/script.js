ec2e5395bc3d201dfa1c71f16fc50f1175cf8290
Вячеслав <slagrach@gmail.com>
2020-06-15 22:45:33 +0300

    Update
    v 2.1

--- Geekbrains-lessons/js/script.js
+++ Geekbrains-lessons/js/script.js
@@ -19,13 +19,68 @@ $(document).ready(function () {
     $('.slider__card').slick({
         arrows: true, // стрелки вкдючены
         dots: true, // точки включены
-        slidesToShow: 2, // показывать два слайда
+        slidesToShow: 3, // показывать два слайда
         initialSlide: 0, // с какого слайда начинать
         autoplay: true, // автопрокрутка
         autoplaySpeed: 5000, // автопрокрутка каждые 5сек
         infinite: true, // бесконечная прокрутка
         pauseOnFocus: true,
         pauseOnHover: true,
-        pauseOnDotsHover: true
+        pauseOnDotsHover: true,
+        variableWidth: true,
+        centerMode: true,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: false
+                }
+            }
+        ]
+    });
+});
+
+
+$(function () {
+    $('.minimized').click(function (event) {
+        var i_path = $(this).attr('src');
+        $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
+        $('#magnify').css({
+            left: ($(document).width() - $('#magnify').outerWidth()) / 2,
+            // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
+            top: ($(window).height() - $('#magnify').outerHeight()) / 2
+        });
+        $('#overlay, #magnify').fadeIn('fast');
+    });
+
+    $('body').on('click', '#close-popup, #overlay', function (event) {
+        event.preventDefault();
+
+        $('#overlay, #magnify').fadeOut('fast', function () {
+            $('#close-popup, #magnify, #overlay').remove();
+        });
     });
 });
